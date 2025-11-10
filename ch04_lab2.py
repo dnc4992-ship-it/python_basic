5a561d38e77650aa5dda27bb3e3d125b238257d7
dnc4992-ship-it <dnc4992@gmail.com>
2025-11-11 00:33:35 +0900

    Add files via upload

---
+++ ch04_lab2.py
@@ -0,0 +1,25 @@
+# ch04_lab2.py
+
+n1 = input('input n1: ')
+          
+n1 = int(n1)
+
+           
+n2 = input('input n2: ')
+
+n2 = int(n2)
+           
+
+
+나누기 = n1 / n2 
+
+몫 = n1 // n2 
+
+나머지 = n1 % n2
+
+
+print(f'n1 / n2 = {나누기 : }')
+
+print(f'몫: {몫 : }')
+
+print(f'나머지 = {나머지 : }')
