81ccdda438ccc1b89481c322282c873e8777df45
dnc4992-ship-it <dnc4992@gmail.com>
2025-11-11 16:40:37 +0900

    Add files via upload

---
+++ ch04_lab7.py
@@ -0,0 +1,10 @@
+#  ch04_lab7.py
+
+
+food_cost = int(input("음식값은?") )
+
+food_vat = int(food_cost*1.1)           # 10% vat
+
+print()
+                      
+print(f"음식값으로 총 지불할 금액은{food_vat}원입니다")
