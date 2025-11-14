3214f3ea01cb7f509459f7e9fa14746cd5baf7ec
dnc4992-ship-it <dnc4992@gmail.com>
2025-11-15 08:56:07 +0900

    Add files via upload

--- ch04_lab1.py
+++ ch04_lab1.py
@@ -1,9 +1,28 @@
 # ch04_lab1.py
 
-n1 = input('정수를 입력하세요 : ')
-n2 = input('정수를 입력하세요 : ')
-n1 = int(n1)
-n2 = int(n2)
 
-print (f'{n1}과 {n2}을 더하면 {n1 +n2}입니다.')
-print (f'{
+a = int(input('정수를 입력하세요.'))
+
+print()       
+
+b = int(input('정수를 입력하세요.') )
+
+print()
+
+
+
+a = int(a)
+
+b = int(b)
+
+print()
+
+print(f'{a}과 {b}을 더하면, { a + b}입니다.')
+
+print()
+
+print(f'{a}을 {b}로 나누면 {(a / b)}입니다.')
+
+print()
+
+print(f'{a}을 {b}로 나누면 {(a // b)}입니다.')
