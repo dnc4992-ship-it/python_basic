f230f476c40c52d21b38062f809929bdad6151c8
dnc4992-ship-it <dnc4992@gmail.com>
2025-11-21 16:05:46 +0900

    Add files via upload

--- ch06_lab2.py
+++ ch06_lab2.py
@@ -1,16 +1,20 @@
 # ch06_lab2.py
 
 n = 1       # 횟수
+
 s= 0
 
 
-while n <= 2:
+while n <= 10:
+    
     if n % 2 == 1:  # 홀수일때
+        
        s = s + n
 
     else :
         s = s-n
 
     n = n+1
+    
+    
 print(s)
-
