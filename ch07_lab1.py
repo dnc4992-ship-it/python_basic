041f72437e5ac0f07d1b50f9e9bf70e087b4cc85
dnc4992-ship-it <dnc4992@gmail.com>
2025-11-25 18:11:38 +0900

    Add files via upload

---
+++ ch07_lab1.py
@@ -0,0 +1,17 @@
+
+# ch07_lab1.py
+
+while True:
+    a =int(input('점수를 입력하세요 :'))
+    
+    if a < 0 :
+        break
+
+    if  a % 2 == 1 :
+        print('홀수입니다.')
+
+    else :
+         a == 0
+         print('짝수입니다.')    
+
+  
