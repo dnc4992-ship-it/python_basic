7e40065a6c1fd3efa075065057183a93eed17f49
dnc4992-ship-it <dnc4992@gmail.com>
2025-11-28 17:29:01 +0900

    Add files via upload

---
+++ ch08_lab1.py
@@ -0,0 +1,16 @@
+# ch08_lab1.py
+
+def n(name):
+    print(f'{name}')
+
+def a(add):
+     print(f'{add}')
+
+def t(tel):
+    print(f'{tel}')
+
+n('평촌아이티')
+
+a('www.pitca.co.kr')
+
+t('031-466-9737')
